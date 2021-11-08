e3e229c548ea47fa92bf8bdb6c2d261e13934299
Sahar Oz <saharoz966@gmail.com>
2021-11-08 18:04:50 +0200

    Order entity was created

--- G14_BiteMe/src/Entities/Costumer.java
+++ G14_BiteMe/src/Entities/Costumer.java
@@ -4,6 +4,7 @@ public class Costumer extends User {
 	
 	
 	
+	
 
 	public Costumer(String userName, String Password, String ID, String firstName, String familyName, String Email,
 			String role, String TelephoneNumber) {
--- G14_BiteMe/src/Entities/Order.java
+++ G14_BiteMe/src/Entities/Order.java
@@ -2,4 +2,55 @@ package Entities;
 
 public class Order {
 
+	public Dish[] OrderedDishes;
+
+	public String Restaurants;
+	public String SpecificDetails;
+	public double Discount;
+	public double finalPrice;
+
+	public enum paymantmethod {
+		CREDIT_CARD, BUSINESS_CARD
+	}
+
+	public String getRestaurants() {
+		return Restaurants;
+	}
+
+	public void setRestaurants(String restaurants) {
+		Restaurants = restaurants;
+	}
+
+	public String getSpecificDetails() {
+		return SpecificDetails;
+	}
+
+	public void setSpecificDetails(String specificDetails) {
+		SpecificDetails = specificDetails;
+	}
+
+	public double getDiscount() {
+		return Discount;
+	}
+
+	public void setDiscount(double discount) {
+		Discount = discount;
+	}
+
+	public double getFinalPrice() {
+		return finalPrice;
+	}
+
+	public void setFinalPrice(double finalPrice) {
+		this.finalPrice = finalPrice;
+	};
+
+	public Dish[] getOrderedDishes() {
+		return OrderedDishes;
+	};
+
+	public void setOrderedDishes(Dish[] orderedDishes) {
+		OrderedDishes = orderedDishes;
+	};
+
 }
