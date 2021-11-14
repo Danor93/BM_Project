a8bb84715ad2f2246943cbd4b5b5fa0c78aaa6a3
Sahar Oz <saharoz966@gmail.com>
2021-11-14 18:08:49 +0200

    Merge branch 'master' of https://github.com/Danor93/G14_Project.git

--- G14_BiteMe/src/Entities/Costumer.java
+++ G14_BiteMe/src/Entities/Costumer.java
@@ -2,6 +2,8 @@ package Entities;
 
 public class Costumer extends User {
 	
+	
+	
 	public Costumer(String userName, String Password, String ID, String firstName, String familyName, String Email,
 			String role, String TelephoneNumber) {
 		super(userName, Password, ID, firstName, familyName, Email, role, TelephoneNumber);
---
+++ G14_BiteMe/src/server/GUIServerMain.java
@@ -0,0 +1,16 @@
+package server;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUIServerMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
