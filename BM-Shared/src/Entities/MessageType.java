8ff41eea6299b35277bc654cc0afc6e77309a1bb
Danor93 <danors@gmail.com>
2021-11-30 12:59:40 +0200

    OCSF and Shared

---
+++ BM-Shared/src/Entities/MessageType.java
@@ -0,0 +1,13 @@
+package Entities;
+
+public enum MessageType {
+
+	/*Server Message*/
+	Update_succesfuly,Show_Orders_succ,login,Disconected,loginSystem,
+	
+	/*Client Message*/
+	Update_Orders,Show_Orders,
+	
+	/*Error*/
+	Error
+}
