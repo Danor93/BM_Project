09b0b509dcde31df461ccf0cbe2bf25980856ba7
Danor93 <danors@gmail.com>
2022-01-04 18:18:40 +0200

    final update and add java doc,update the db

---
+++ BM-Client/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"client.controllers"},{"l":"Interfaces"},{"l":"main"},{"l":"Parsing"}]
---
+++ BM-Server/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"controllers"},{"l":"extra"},{"l":"Interfaces"},{"l":"main"},{"l":"Parsing"},{"l":"querys"}]
